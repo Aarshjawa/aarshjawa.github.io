4d3edb69c83aeee284ebc23f1207dc96d67137ee
Aarsh jawa <53709466+Aarshjawa@users.noreply.github.com>
2025-05-01 19:07:06 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["Security Operations Center", "Digital Forensics", "Incident Response", "Articles | Research papers | Writing", "Digital Risk Protection", "Threat Hunting"],
+    strings: ["Security Operations Center", "Digital Forensics", "Incident Response", "Articles | Research papers | Writing", "Digital Risk Protection", "Threat Hunting - Detection"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
